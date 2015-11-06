ac6c7edb809cf4440194cbf52c971cfe8e638e38
phtn <phtn458@gmail.com>
2015-11-06 00:34:44 -0500

    fb login

--- server/accounts.js
+++ server/accounts.js
@@ -19,7 +19,7 @@ ServiceConfiguration.configurations.upsert(
   	secret: "783b9bf9a3aff6632adeca1e4bab7717" 
   } }
 );
-
+/*
 // localhost
 ServiceConfiguration.configurations.upsert(
   { service: "facebook" },
@@ -28,3 +28,4 @@ ServiceConfiguration.configurations.upsert(
     secret: "6f5cda4a2ff9bd8916553a99645061a4" 
   } }
 );
+*/
